51a7f12a9a2ad08c9cbd0d36d31155c0357bfc3a
Matthew Collins <m.collins@etoncollege.org.uk>
2023-12-11 20:38:34 +0000

    Initial Commit

---
+++ Othello/OthelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthelloApp.swift
+//  Othello
+//
+//  Created by Collins, Matthew - MC on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct OthelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
